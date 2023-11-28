13e4df672ba4b6f0d836507403c0606a37bbea77
OlgaCh <chamina51@mail.ru>
2023-11-28 16:23:05 +0300

    add new tests

--- src/test/java/ru/netology/javaqa/PostManagerTest.java
+++ src/test/java/ru/netology/javaqa/PostManagerTest.java
@@ -1,3 +1,5 @@
+package ru.netology.javaqa;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import ru.netology.javaqa.PosterManager;
@@ -6,24 +8,26 @@ public class PostManagerTest {
 
 
     @Test
-    public void shouldAdd0Poster(){
+    public void shouldAdd0Poster() {
         PosterManager manager = new PosterManager();
 
         String[] expected = {};
         String[] actual = manager.findAll();
-        Assertions.assertArrayEquals(expected,actual);
+        Assertions.assertArrayEquals(expected, actual);
     }
+
     @Test
-    public void shouldAdd1Poster(){
+    public void shouldAdd1Poster() {
         PosterManager manager = new PosterManager();
         manager.savePoster("Poster1");
 
         String[] expected = {"Poster1"};
         String[] actual = manager.findAll();
-        Assertions.assertArrayEquals(expected,actual);
+        Assertions.assertArrayEquals(expected, actual);
     }
+
     @Test
-    public void shouldAdd5Poster(){
+    public void shouldAdd5Poster() {
         PosterManager manager = new PosterManager();
         manager.savePoster("Poster1");
         manager.savePoster("Poster2");
@@ -31,13 +35,13 @@ public class PostManagerTest {
         manager.savePoster("Poster4");
         manager.savePoster("Poster5");
 
-        String[] expected = {"Poster1","Poster2","Poster3","Poster4","Poster5"};
+        String[] expected = {"Poster1", "Poster2", "Poster3", "Poster4", "Poster5"};
         String[] actual = manager.findAll();
-        Assertions.assertArrayEquals(expected,actual);
+        Assertions.assertArrayEquals(expected, actual);
     }
 
     @Test
-    public void shouldRevers5Poster(){
+    public void shouldRevers5Poster() {
         PosterManager manager = new PosterManager();
         manager.savePoster("Poster1");
         manager.savePoster("Poster2");
@@ -45,34 +49,37 @@ public class PostManagerTest {
         manager.savePoster("Poster4");
         manager.savePoster("Poster5");
 
-        String[] expected = {"Poster5","Poster4","Poster3","Poster2","Poster1"};
+        String[] expected = {"Poster5", "Poster4", "Poster3", "Poster2", "Poster1"};
         String[] actual = manager.findLast();
-        Assertions.assertArrayEquals(expected,actual);
+        Assertions.assertArrayEquals(expected, actual);
     }
+
     @Test
-    public void shouldRevers3Poster(){
+    public void shouldRevers3Poster() {
         PosterManager manager = new PosterManager();
         manager.savePoster("Poster1");
         manager.savePoster("Poster2");
         manager.savePoster("Poster3");
 
 
-        String[] expected = {"Poster3","Poster2","Poster1"};
+        String[] expected = {"Poster3", "Poster2", "Poster1"};
         String[] actual = manager.findLast();
-        Assertions.assertArrayEquals(expected,actual);
+        Assertions.assertArrayEquals(expected, actual);
     }
+
     @Test
-    public void shouldRevers1Poster(){
+    public void shouldRevers1Poster() {
         PosterManager manager = new PosterManager();
         manager.savePoster("Poster1");
 
 
         String[] expected = {"Poster1"};
         String[] actual = manager.findLast();
-        Assertions.assertArrayEquals(expected,actual);
+        Assertions.assertArrayEquals(expected, actual);
     }
+
     @Test
-    public void shouldRevers7Poster(){
+    public void shouldRevers7PosterIfLimit7() {
         PosterManager manager = new PosterManager(7);
 
         manager.savePoster("Poster1");
@@ -83,9 +90,40 @@ public class PostManagerTest {
         manager.savePoster("Poster6");
         manager.savePoster("Poster7");
 
-        String[] expected = {"Poster7","Poster6","Poster5","Poster4","Poster3","Poster2","Poster1"};
+        String[] expected = {"Poster7", "Poster6", "Poster5", "Poster4", "Poster3", "Poster2", "Poster1"};
         String[] actual = manager.findLast();
-        Assertions.assertArrayEquals(expected,actual);
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldRevers3PosterifLimit5() {
+        PosterManager manager = new PosterManager(5);
+
+        manager.savePoster("Poster1");
+        manager.savePoster("Poster2");
+        manager.savePoster("Poster3");
+
+
+        String[] expected = {"Poster3", "Poster2", "Poster1"};
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReversLastPosterIfLimit5() {
+        PosterManager manager = new PosterManager(5);
+
+        manager.savePoster("Poster1");
+        manager.savePoster("Poster2");
+        manager.savePoster("Poster3");
+        manager.savePoster("Poster4");
+        manager.savePoster("Poster5");
+        manager.savePoster("Poster6");
+        manager.savePoster("Poster7");
+
+        String[] expected = {"Poster7", "Poster6", "Poster5", "Poster4", "Poster3"};
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
     }
 
 }
